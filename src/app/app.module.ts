39f5f7f886fae7bd96b19626fbc54d8c91541308
Miguel Montes <68451358+MiguelAngelMC@users.noreply.github.com>
2023-05-17 00:51:34 -0700

    fix type effect and grid
    Se agregó el efecto de typing y se acomodó el grid para que se adapte en desktop y mobile.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
+//Dependency
+import {NgxTypedJsModule} from 'ngx-typed-js';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,8 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxTypedJsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
